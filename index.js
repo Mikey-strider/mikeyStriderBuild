8df50083eb1f658fc07a886ef996c10e39d2a6a6
Dromen2001 <dromen2001@gmail.com>
2025-01-05 19:47:01 -0800

    Beginning portfolio build

---
+++ index.js
@@ -0,0 +1,12 @@
+const mainBuild = document.querySelector('.mikey-builds');
+
+
+mainBuild.innerHTML = `
+  <ul navigation>
+    <a><li>About Me</li></a>
+    <a><li>My Projects</li></a>
+    <a><li>Education</li></a>
+    <a><li>Specialties</li></a>
+    <a><li>Contact Me</li></a>
+  </ul>
+`;
